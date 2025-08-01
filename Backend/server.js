64bd51ca8876b47a15ecc870ee214364dcb6929a
sahilwalia04 <21-m-1792@rayatbahrauniversity.edu.in>
2025-08-01 14:44:27 +0530

    port change

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://http://13.49.73.215:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
